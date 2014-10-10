e1036899049e927f28bb4a070890dc8f610ddfea
Mike Simons <msimons@inviqa.com>
2014-10-10 18:04:39 +0200

    Ensure openssl-customisations are applied to all platforms

--- config/software/openssl-customization.rb
+++ config/software/openssl-customization.rb
@@ -30,39 +30,37 @@ else
 end
 
 build do
-  if windows?
-    block "Add OpenSSL customization file" do
-      # gets directories for RbConfig::CONFIG and sanitizes them.
-      def get_sanitized_rbconfig(config)
-        ruby = windows_safe_path("#{install_dir}/embedded/bin/ruby")
+  block "Add OpenSSL customization file" do
+    # gets directories for RbConfig::CONFIG and sanitizes them.
+    def get_sanitized_rbconfig(config)
+      ruby = windows_safe_path("#{install_dir}/embedded/bin/ruby")
 
-        config_dir = Bundler.with_clean_env do
-          command_output = %x|#{ruby} -rrbconfig -e "puts RbConfig::CONFIG['#{config}']"|.strip
-          windows_safe_path(command_output)
-        end
-
-        if config_dir.nil? || config_dir.empty?
-          raise "could not determine embedded ruby's RbConfig::CONFIG['#{config}']"
-        end
-
-        config_dir
+      config_dir = Bundler.with_clean_env do
+        command_output = %x|#{ruby} -rrbconfig -e "puts RbConfig::CONFIG['#{config}']"|.strip
+        windows_safe_path(command_output)
       end
 
-      embedded_ruby_site_dir = get_sanitized_rbconfig('sitelibdir')
-      embedded_ruby_lib_dir  = get_sanitized_rbconfig('rubylibdir')
-
-      source_ssl_env_hack      = File.join(project_dir, "windows", "ssl_env_hack.rb")
-      destination_ssl_env_hack = File.join(embedded_ruby_site_dir, "ssl_env_hack.rb")
-
-      copy(source_ssl_env_hack, destination_ssl_env_hack)
-
-      # Unfortunately there is no patch on windows, but luckily we only need to append a line to the openssl.rb
-      # to pick up our script which find the CA bundle in omnibus installations and points SSL_CERT_FILE to it
-      # if it's not already set
-      source_openssl_rb = File.join(embedded_ruby_lib_dir, "openssl.rb")
-      File.open(source_openssl_rb, "a") do |f|
-        f.write("\nrequire 'ssl_env_hack'\n")
+      if config_dir.nil? || config_dir.empty?
+        raise "could not determine embedded ruby's RbConfig::CONFIG['#{config}']"
       end
+
+      config_dir
+    end
+
+    embedded_ruby_site_dir = get_sanitized_rbconfig('sitelibdir')
+    embedded_ruby_lib_dir  = get_sanitized_rbconfig('rubylibdir')
+
+    source_ssl_env_hack      = File.join(project_dir, "windows", "ssl_env_hack.rb")
+    destination_ssl_env_hack = File.join(embedded_ruby_site_dir, "ssl_env_hack.rb")
+
+    copy(source_ssl_env_hack, destination_ssl_env_hack)
+
+    # Unfortunately there is no patch on windows, but luckily we only need to append a line to the openssl.rb
+    # to pick up our script which find the CA bundle in omnibus installations and points SSL_CERT_FILE to it
+    # if it's not already set
+    source_openssl_rb = File.join(embedded_ruby_lib_dir, "openssl.rb")
+    File.open(source_openssl_rb, "a") do |f|
+      f.write("\nrequire 'ssl_env_hack'\n")
     end
   end
 end
